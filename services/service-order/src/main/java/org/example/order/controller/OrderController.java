d44f43bfdb8d5adebec71da43b87fc69a77a41d1
Nitro <3103148441@qq.com>
2025-07-11 01:03:23 +0800

    13.配置中心 - 基本用法，启动Nacos、引入依赖、application.properties配置、在Nacos控制台创建data-id配置信息

--- services/service-order/src/main/java/org/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/org/example/order/controller/OrderController.java
@@ -3,15 +3,28 @@ package org.example.order.controller;
 import org.example.order.bean.Order;
 import org.example.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope // 自动刷新
 @RestController
 public class OrderController {
 
     @Autowired
     OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config(){
+        return "timeout:" + orderTimeout + " auto-confirm:" + orderAutoConfirm;
+    }
 
     @GetMapping("/create")
     public Order createOrder(@RequestParam("userId") Long userId,
--- services/service-product/src/main/java/org/example/product/controller/ProductController.java
+++ services/service-product/src/main/java/org/example/product/controller/ProductController.java
@@ -3,10 +3,12 @@ package org.example.product.controller;
 import org.example.product.bean.Product;
 import org.example.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
 
