4e66c1f8d3cbf14e24bb23d37e696f71097c5f54
Nitro <3103148441@qq.com>
2025-07-11 01:55:45 +0800

    14.配置中心 - 配置监听 NacosConfigManager

--- services/service-order/src/main/java/org/example/order/OrderMainApplication.java
+++ services/service-order/src/main/java/org/example/order/OrderMainApplication.java
@@ -1,11 +1,43 @@
 package org.example.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+    //1、项目启动就监听配置文件变化
+    //2、发生变化后拿到变化值
+    //3、发送请求
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager){
+        // 这里函数式参数可以简写
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(4);
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    System.out.println("变化的配置信息："+configInfo);
+                    System.out.println("邮件通知。。。");
+                }
+            });
+            System.out.println("监听到配置文件变化");
+        };
     }
 }
--- services/service-order/src/main/java/org/example/order/properties/OrderProperties.java
+++ services/service-order/src/main/java/org/example/order/properties/OrderProperties.java
@@ -1,7 +1,6 @@
 package org.example.order.properties;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
